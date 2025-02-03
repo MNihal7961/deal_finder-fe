550bb8362ac4ef7c7087651c8d2d8270a944cfc7
MNihal7961 <mnihal7961@gmail.com>
2025-02-03 23:47:53 +0530

    mongo db connection added

---
+++ lib/mongoose.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose"
+
+let isConnected = false;
+export const connectToDB = async () => {
+    mongoose.set('strictQuery', true)
+
+    if(!process.env.MONGODB_URI) return console.log("MONGODB_URI not defined");
+
+    if(isConnected) return console.log("Already connected to MongoDB");
+
+    try{
+        await mongoose.connect(process.env.MONGODB_URI);
+        isConnected = true;
+        console.log("Connected to MongoDB");
+    }catch(error:any){
+        console.log(error);
+        throw new Error("Failed to connect to MongoDB :"+ error.message);
+    }
+}
