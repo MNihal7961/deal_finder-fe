ad5b39285694149c55292584b8a5d59d3c3cc038
MNihal7961 <mnihal7961@gmail.com>
2025-02-03 21:21:50 +0530

    search bar added

---
+++ components/SearchBar.tsx
@@ -0,0 +1,16 @@
+"use client"
+import React from 'react'
+
+const SearchBar = () => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        
+    }
+  return (
+    <form  onSubmit={handleSubmit} className='flex flex-wrap gap-4 mt-12'>
+        <input type="text" placeholder='Enter product link' className='searchbar-input' />
+        <button type='submit' className='searchbar-btn'>Search</button>
+    </form>
+  )
+}
+
+export default SearchBar
