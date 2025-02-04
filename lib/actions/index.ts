a4c94c95789d6e1ad4d8a4fa73c3cb581da04944
MNihal7961 <mnihal7961@gmail.com>
2025-02-05 00:40:26 +0530

    fetch product methods added

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,23 +1,72 @@
-"use server"
-
+"use server";
+import { revalidatePath } from "next/cache";
+import { Product } from "@/types";
+import ProductModel from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { scrapAmazonProduct } from "../scraper";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 
 export async function scrapAndStoreProduct(productLink: string) {
-    if(!productLink) return;
+  if (!productLink) return;
 
-    try{
-        connectToDB();
-        const scrapedProduct = await scrapAmazonProduct(productLink);
+  try {
+    connectToDB();
+    const scrapedProduct = await scrapAmazonProduct(productLink);
 
-        if(!scrapedProduct) return;
+    if (!scrapedProduct) return;
 
-        const product = {
-            ...scrapedProduct,
-            url:productLink
-        }
-    }catch(error: any){
-        console.log("🚀 ~ scrapAndStoreProduct ~ error:", error)
-        throw new Error(`Failed to scrap and store product: ${error.message}`);
+    let product = scrapedProduct;
+
+    const existingProduct: Product | null = await ProductModel.findOne({
+      url: scrapedProduct.url,
+    });
+
+    if (existingProduct) {
+      const updatedProductPriceHistory = [
+        ...existingProduct.priceHistory,
+        { price: scrapedProduct.currentPrice },
+      ];
+
+      product = {
+        ...scrapedProduct,
+        priceHistory: updatedProductPriceHistory as any,
+        lowestPrice: getLowestPrice(updatedProductPriceHistory),
+        highestPrice: getHighestPrice(updatedProductPriceHistory),
+        averagePrice: getAveragePrice(updatedProductPriceHistory),
+      };
     }
+
+    const newProduct = await ProductModel.findOneAndUpdate(
+      {
+        url: scrapedProduct?.url,
+      },
+      product,
+      { upsert: true, new: true }
+    );
+
+    revalidatePath(`/products/${newProduct?._id}`);
+  } catch (error: any) {
+    throw new Error(`Failed to scrap and store product: ${error.message}`);
+  }
+}
+
+export async function getProductById(productId: string) {
+  try {
+    connectToDB();
+    const product = await ProductModel.findOne({ _id: productId });
+    if (!product) return null;
+    return product;
+  } catch (error: any) {
+    throw new Error(`Failed to get product by id: ${error.message}`);
+  }
+}
+
+export async function getAllProducts() {
+  try {
+    connectToDB();
+    const products: Product[] = await ProductModel.find({});
+    return products;
+  } catch (error: any) {
+    throw new Error(`Failed to get all products: ${error.message}`);
+  }
 }
